194bb1b595c4c1c321b1256f85002b57b3d8bf08
Elquiasj <elquiasj@gmail.com>
2025-12-09 18:41:03 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/esource-hosting/",
+  base: "/resource-hosting/",
   server: {
     port: 3001,
     open: true,
